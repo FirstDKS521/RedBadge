697a9ce682815097178331dc6a2671e565ad2971
duks <duks@robooot.com>
2016-09-18 12:06:21 +0800

    Initial Commit

---
+++ RedBadge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedBadge
+//
+//  Created by aDu on 16/9/18.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
